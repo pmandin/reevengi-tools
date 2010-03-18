9012f629d0d1d44766444b5e7f85733bf75ab39b
Patrice Mandin <patmandin@gmail.com>
2010-03-18 17:00:21 +0000

    File extraction enableable

--- src/iso_search.c
+++ src/iso_search.c
@@ -30,14 +30,22 @@
 
 /*--- Defines ---*/
 
+/*#define EXTRACT_FILES	1*/
+
 #define MAGIC_TIM	0x10
 #define TIM_TYPE_4	8
 #define TIM_TYPE_8	9
 #define TIM_TYPE_16	2
 
+#define FILE_TIM	0
+#define FILE_EMD	1
+
+#define DATA_LENGTH 2048
+
 /*--- Functions prototypes ---*/
 
 int browse_iso(const char *filename);
+void extract_file(SDL_RWops *src, Uint32 start, Uint32 end, int file_type);
 
 /*--- Functions ---*/
 
@@ -67,7 +75,8 @@ int browse_iso(const char *filename)
 	SDL_RWops *src;
 	Uint32 length, blocks, offset;
 	Uint8 data[2352];
-	int i;
+	int i, extract_flag = 0, file_type = FILE_TIM, new_file_type = FILE_TIM;
+	Uint32 start=0,end=0;
 
 	src = SDL_RWFromFile(filename, "rb");
 	if (!src) {
@@ -86,6 +95,7 @@ int browse_iso(const char *filename)
 		return 1;
 	}
 
+	start = end = 0;
 	for (i=0; i<blocks; i++) {
 		Uint32 value;
 
@@ -108,12 +118,21 @@ int browse_iso(const char *filename)
 			switch(value) {
 				case TIM_TYPE_4:
 					printf("Sector %d (offset 0x%08x): 4 bits TIM image\n",i,offset);
+					end = i;
+					extract_flag = 1;
+					new_file_type = FILE_TIM;
 					break;
 				case TIM_TYPE_8:
 					printf("Sector %d (offset 0x%08x): 8 bits TIM image\n",i,offset);
+					end = i;
+					extract_flag = 1;
+					new_file_type = FILE_TIM;
 					break;
 				case TIM_TYPE_16:
 					printf("Sector %d (offset 0x%08x): 16 bits TIM image\n",i,offset);
+					end = i;
+					extract_flag = 1;
+					new_file_type = FILE_TIM;
 					break;
 			}
 		} else if (value < 512<<10) {
@@ -125,10 +144,67 @@ int browse_iso(const char *filename)
 
 			if (value==0x0f) {
 				printf("Sector %d (offset 0x%08x): EMD model (maybe)\n",i,offset);
+				end = i;
+				extract_flag = 1;
+				new_file_type = FILE_EMD;
 			}
+		}
+
+		if ((start>16000) && (end!=0) && extract_flag) {
+#ifdef EXTRACT_FILES
+			extract_file(src, start,end, file_type);
+#endif
+			extract_flag = 0;
+			file_type = new_file_type;
+		}
+		if (end!=0) {
+			start = end;
+			end = 0;
 		}
 	}
 
 	SDL_RWclose(src);
 	return 0;
+}
+
+void extract_file(SDL_RWops *src, Uint32 start, Uint32 end, int file_type)
+{
+	Uint8 *buffer;
+	Uint32 length = DATA_LENGTH * (end-start);
+	int i;
+	char filename[16];
+	char *fileext = "%08x.bin";
+	SDL_RWops *dst;
+
+	buffer = (Uint8 *) malloc(length);
+	if (!buffer) {
+		return;
+	}
+
+	for (i=0; i<end-start; i++) {	
+		SDL_RWseek(src, ((start+i)*2352)+16+8, RW_SEEK_SET);
+		SDL_RWread(src, &buffer[i*DATA_LENGTH], DATA_LENGTH, 1);
+	}
+
+	switch(file_type) {
+		case FILE_TIM:
+			fileext = "%08x.tim";
+			break;
+		case FILE_EMD:
+			fileext = "%08x.emd";
+			break;
+	}
+	sprintf(filename, fileext, start);
+
+	dst = SDL_RWFromFile(filename, "wb");
+	if (!dst) {
+		fprintf(stderr, "Can not create %s for writing\n", filename);
+		free(buffer);
+		return;
+	}
+
+	SDL_RWwrite(dst, buffer, length, 1);
+	SDL_RWclose(dst);
+
+	free(buffer);
 }
