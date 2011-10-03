19a93d825dc2f2102ba90101f3fbe660b6486388
Patrice Mandin <patmandin@gmail.com>
2011-10-03 06:09:15 +0000

    EMD1 armature

--- src/emd2xml.c
+++ src/emd2xml.c
@@ -19,6 +19,7 @@
 */
 
 #include <libxml/xmlversion.h>
+#include <libxml/xmlwriter.h>
 #include <SDL.h>
 
 #ifdef HAVE_CONFIG_H
@@ -27,14 +28,20 @@
 
 #include "file_functions.h"
 #include "emd_common.h"
+#include "emd1.h"
 
 /*--- Functions prototypes ---*/
 
 int emdToXml(const char *filename);
 int getEmdVersion(Uint8 *src, Uint32 srcLen);
-int emd1ToXml(Uint8 *src, Uint32 srcLen);
-int emd2ToXml(Uint8 *src, Uint32 srcLen);
-int emd3ToXml(Uint8 *src, Uint32 srcLen);
+
+int emd1ToXml(Uint8 *src, Uint32 srcLen, xmlDoc *doc);
+void emd1AddSkeleton(Uint8 *src, Uint32 srcLen, xmlNodePtr root);
+void emd1AddArmature(xmlNodePtr root, emd1_skel_data_t *emd_skel_data, emd_vertex3_t *emd_skel_relpos, int start_mesh);
+
+int emd2ToXml(Uint8 *src, Uint32 srcLen, xmlDoc *doc);
+
+int emd3ToXml(Uint8 *src, Uint32 srcLen, xmlDoc *doc);
 
 /*--- Functions ---*/
 
@@ -71,6 +78,7 @@ int emdToXml(const char *filename)
 	Uint8 *srcBuffer;
 	int srcBufLen, gameVersion;
 	int retval = 1;
+	xmlDoc *doc;
 
 	/* Read file in memory */
 	src = SDL_RWFromFile(filename, "rb");
@@ -93,20 +101,28 @@ int emdToXml(const char *filename)
 
 	/* Detect which game version */
 	gameVersion = getEmdVersion(srcBuffer, srcBufLen);
+
+	doc = xmlNewDoc(BAD_CAST "1.0");
 	switch(gameVersion) {
 		case 1:
-			retval = emd1ToXml(srcBuffer, srcBufLen);
+			retval = emd1ToXml(srcBuffer, srcBufLen, doc);
 			break;
 		case 2:
-			retval = emd2ToXml(srcBuffer, srcBufLen);
+			retval = emd2ToXml(srcBuffer, srcBufLen, doc);
 			break;
 		case 3:
-			retval = emd3ToXml(srcBuffer, srcBufLen);
+			retval = emd3ToXml(srcBuffer, srcBufLen, doc);
 			break;
 		default:
 			retval = 1;
 			break;
 	}
+
+	/* Save if OK */
+	if (!retval) {
+		xmlSaveFormatFileEnc("re1.xml", doc, "UTF-8", 1);
+	}
+	xmlFreeDoc(doc);
 
 	return retval;
 }
@@ -134,15 +150,85 @@ int getEmdVersion(Uint8 *src, Uint32 srcLen)
 
 /*--- RE1 EMD ---*/
 
-int emd1ToXml(Uint8 *src, Uint32 srcLen)
+int emd1ToXml(Uint8 *src, Uint32 srcLen, xmlDoc *doc)
 {
+	xmlNodePtr root, node;
+
 	printf("Detected RE1 EMD file\n");
-	return 1;
+	root = xmlNewNode(NULL, BAD_CAST "emd");	
+	xmlNewProp(root, BAD_CAST "version", BAD_CAST "1");
+	xmlDocSetRootElement(doc, root);
+
+	emd1AddSkeleton(src, srcLen, root);
+
+	/* Animations */
+	node = xmlNewNode(NULL, BAD_CAST "animation");
+	xmlAddChild(root, node);
+
+	/* Meshes */
+	node = xmlNewNode(NULL, BAD_CAST "mesh");
+	xmlAddChild(root, node);
+
+	/* TIM image */
+	node = xmlNewNode(NULL, BAD_CAST "tim");
+	xmlAddChild(root, node);
+	xmlNewProp(node, BAD_CAST "filename", BAD_CAST "texture.tim");
+
+	return 0;
+}
+
+void emd1AddSkeleton(Uint8 *src, Uint32 srcLen, xmlNodePtr root)
+{
+	xmlNodePtr node;
+	Uint32 *hdr_offsets;
+	Uint8 *src_skel;
+	emd_skel_header_t *emd_skel_header;
+	emd_vertex3_t *emd_skel_relpos;
+	emd1_skel_data_t *emd_skel_data;
+
+	hdr_offsets = (Uint32 *) (&src[srcLen-16]);
+	src_skel = &src[SDL_SwapLE32(hdr_offsets[EMD1_SKELETON])];
+
+	emd_skel_header = (emd_skel_header_t *) src_skel;
+	emd_skel_relpos = (emd_vertex3_t *) (&src_skel[sizeof(emd_skel_header_t)]);
+	emd_skel_data = (emd1_skel_data_t *) (&src_skel[SDL_SwapLE16(emd_skel_header->relpos_offset)]);
+
+	node = xmlNewNode(NULL, BAD_CAST "skeleton");
+	xmlAddChild(root, node);
+
+	emd1AddArmature(node, emd_skel_data, emd_skel_relpos, 0);
+}
+
+void emd1AddArmature(xmlNodePtr root, emd1_skel_data_t *emd_skel_data, emd_vertex3_t *emd_skel_relpos, int start_mesh)
+{
+	xmlNodePtr node;
+	xmlChar buf[32];
+	Uint16 num_mesh = SDL_SwapLE16(emd_skel_data[start_mesh].num_mesh);
+	Uint16 offset = SDL_SwapLE16(emd_skel_data[start_mesh].offset);
+	Uint8 *armature = (Uint8 *) emd_skel_data;
+	int i;
+
+	node = xmlNewNode(NULL, BAD_CAST "armature");
+	xmlAddChild(root, node);
+
+	xmlStrPrintf(buf, sizeof(buf), BAD_CAST "%d", start_mesh);
+	xmlNewProp(node, BAD_CAST "mesh", buf);
+
+	xmlStrPrintf(buf, sizeof(buf), BAD_CAST "%d", SDL_SwapLE16(emd_skel_relpos[start_mesh].x));
+	xmlNewProp(node, BAD_CAST "x", buf);
+	xmlStrPrintf(buf, sizeof(buf), BAD_CAST "%d", SDL_SwapLE16(emd_skel_relpos[start_mesh].y));
+	xmlNewProp(node, BAD_CAST "y", buf);
+	xmlStrPrintf(buf, sizeof(buf), BAD_CAST "%d", SDL_SwapLE16(emd_skel_relpos[start_mesh].z));
+	xmlNewProp(node, BAD_CAST "z", buf);
+
+	for (i=0; i<num_mesh; i++) {
+		emd1AddArmature(node, emd_skel_data, emd_skel_relpos, armature[offset+i]);
+	}
 }
 
 /*--- RE2 EMD ---*/
 
-int emd2ToXml(Uint8 *src, Uint32 srcLen)
+int emd2ToXml(Uint8 *src, Uint32 srcLen, xmlDoc *doc)
 {
 	printf("Detected RE2 EMD file\n");
 	return 1;
@@ -150,7 +236,7 @@ int emd2ToXml(Uint8 *src, Uint32 srcLen)
 
 /*--- RE3 EMD ---*/
 
-int emd3ToXml(Uint8 *src, Uint32 srcLen)
+int emd3ToXml(Uint8 *src, Uint32 srcLen, xmlDoc *doc)
 {
 	printf("Detected RE3 EMD file\n");
 	return 1;
